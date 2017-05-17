05fff4661de688ad5c3738c98533c4301b992154
Jayakumar Vinayagam <jayakumar.vinayagam@rrd.com>
2017-05-17 23:31:28 +0530

    Empty projects added for some training session.

---
+++ IDCTechChampions/TrainingStartUp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace TrainingStartUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to RRD IDC Tech Champions");
+
+            Console.ReadKey(true);
+        }
+    }
+}
